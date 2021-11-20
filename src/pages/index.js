65b85fab92e2f693e859771944da17752d70f3b9
ROCA Philippe <roca.philippe63@gmail.com>
2021-11-20 02:03:21 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Discover The Fable of Etheria</h1>
+    <p>Choose your side between the Fundation & The Alliance.</p>
+    <p>Upgrace your status and be the main character of your own story.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
